9c89d747ffd7ae5cc4fa381d0df255fe8c59a6ad
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2024-10-09 19:41:56 +0700

    first name

---
+++ src/remotedestopserver/Start.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package remotedestopserver;
+
+/**
+ *
+ * @author TUẤN QUỲNH CODER
+ */
+public class Start {
+    public static void main(String[] args) {
+        SetPassword frame1 = new SetPassword();
+        frame1.setSize(300,80);
+        frame1.setLocation(500,300);
+        frame1.setVisible(true);
+    }
+    
+}
